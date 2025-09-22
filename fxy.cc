5e4d1f88b79ef0f465195fb9691f49ba0df85f55
katu <6224025@ed.tus.ac.jp>
2025-09-22 13:40:28 +0900

    fxy.cc完成

--- fxy.cc
+++ fxy.cc
@@ -5,6 +5,23 @@
 using namespace std;
 
 int main(){
+    double x,y,z;
 
-    
+
+ofstream fout;
+fout.open("fxy.data");
+
+for (int j=0 ; j<=40; j++){
+    y=-2.0+0.1*j;
+    for (int i=0 ; i<=40; i++){
+        x=-2.0+0.1*i;
+        z=exp(x*x+y*y);
+        fout<<setprecision(16)<<x<<" "<<y<<" "<<z<<endl;
+    }
+    fout<<endl;
+}
+
+fout.close();
+return 0;
+
 }
