d571d5b3532cfac61e0f6e94255ff9e6caf21a96
katu <6224025@ed.tus.ac.jp>
2025-09-23 14:01:10 +0900

    うおおおおおお

---
+++ kadai4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+ 
+    int c[4]={0,1,2,3};
+
+    cout << "c="    << c << endl;
+    cout << "&c[0]=" << &c[0] << endl;
+    cout << "c[0]=" << c[0] << endl;
+    
+
+}
