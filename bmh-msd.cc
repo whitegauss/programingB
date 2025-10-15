9f5d196593bf7179e1234eb9b8ea47acfa532ef8
6224025 <6224025@ed.tus.ac.jp>
2025-10-16 00:50:43 +0900

    ???1111111???

--- bmh-msd.cc
+++ bmh-msd.cc
@@ -10,34 +10,35 @@
 using namespace std;
 int main(){
     srand(time(NULL));
-    int N=100000;
+    int N=10000;
     double Pi=4*atan(1.0),X=0.0,Y=0.0;
-    std::vector<double> msd(N, 0.0);
-    double DT=0.01,dw,x,y;
-    int D=1,k=2;
-    x=0.,y=0.;
-    fstream fout("msd.txt");
+    vector<double> msd(N, 0.0);
+    double DT=0.001,dw,x;
+    double  D=1.0,k=2.0;
+    x=0.;
+    
+    fstream fout("msd.txt", ios::out | ios::trunc);
 
-    msd[0]=0.0;
-    for(int j=0;j<N/1000;j++) {
+
+    for(int j=0;j<100;j++) {
         x=0.0;
         for (int i=0;i<N;i++){
             do {
                 X = (double)rand() / RAND_MAX;
             } while (X == 0.0);
             Y = (double)rand() / RAND_MAX;
-            dw = sqrt(-2*log(X)) * sin(2*Pi*Y) * sqrt(2*D*DT);
+            dw = sqrt(-2.0*log(X)) * cos(2.0*Pi*Y) * sqrt(2.0*D*DT);
             x = x - k * x * DT + dw;
-            if(j==0){
-                msd[i] = pow(x,2);
+            if(j!=0){
+            msd[i] = msd[i]+pow(x,2);
             }else{
-                msd[i] = msd[i]+pow(x,2);
+            msd[i] = pow(x,2);
             }
         }
     }
     for (int i=0; i<N; i++) {
     double time = i * DT;
-    double average_msd = msd[i] / (N / 1000.0); 
+    double average_msd = msd[i] / 100.0; 
     fout << time << " " << average_msd << endl;
 }
    fout.close(); 
--- langevin-msd.cc
+++ langevin-msd.cc
@@ -5,9 +5,44 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
 int main(){
+    srand(time(NULL));
+    int N=pow(10,4);
+    double Pi=4*atan(1.0),X=0.0,Y=0.0;
+    double K_BT=1,m=1,zeta=2;
+    vector<double> msd(N, 0.0);
+    double DT=0.0001,dw,x=0,v=0;
+    double D=K_BT*zeta;
 
+    fstream fout("msd.txt", ios::out | ios::trunc);
+
+    for (int j=0;j<100;j++) {
+        x=0.0,v=0.0;
+        for(int i=0;i<N;i++){
+            do {
+                X = (double)rand() / RAND_MAX;
+            } while (X == 0.0);
+            Y=(double)rand()/RAND_MAX;
+            dw=sqrt(-2*log(X))*sqrt(2.0*DT*D)*cos(2*Pi*Y);
+            v=v-zeta*v*DT/m+dw/m;
+            x=x+v*DT;
+            if(j==0){
+                msd[i] = pow(x,2);
+            }else{
+                msd[i] = msd[i]+pow(x,2);
+            }
+        }
+    }
+
+    for (int i=0; i<N; i++) {
+    double time = i * DT;
+    double average_msd = msd[i] / 100; 
+    fout << time << " " << average_msd << endl;
+    }
+ fout.close(); 
+   return 0;
 }
